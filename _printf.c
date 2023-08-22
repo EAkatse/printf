d0eb36fb771d68f21add7a57522ab0d0979490c4
Hadi Ahmad <hadiahmad676@gmail.com>
2023-08-22 16:07:18 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,83 +1,133 @@
-#include "main.h"
-/**
- * print_char - prints a single character
- * @c: the character to be printed
- * @char_print: pointer to the counter for printed characters
- */
-void print_char(char c, int *char_print)
-{
-	write(1, &c, 1);
-	(*char_print)++;
-}
-
-/**
- * print_string - Prints a string and updates char_print counter
- * @str: The string to be printed
- * @char_print: Pointer to the counter for printed characters
- */
-void print_string(char *str, int *char_print)
-{
-	int len_str = 0;
-
-	while (str[len_str] != '\0')
-		len_str++;
-	write(1, str, len_str);
-	(*char_print) += len_str;
-}
-
-/**
- * _printf - our version of printf to print data of all formats
- * @format: Input string
- * @...: Variadic arguments
- * Return: Number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	int char_print = 0;
-	va_list list_args;
-
-	va_start(list_args, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			print_char(*format, &char_print);
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				return (1);
-			if (*format == ' ')
-			{
-				if ((*format + 1) == '\0')
-					return (-1);
-			}
-			if (*format == '%')
-			{
-				print_char('%', &char_print);
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(list_args, int);
-
-				print_char(c, &char_print);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list_args, char*);
-
-				if (str == NULL)
-					str = "(null)";
-				print_string(str, &char_print);
-			}
-		}
-		format++;
-	}
-	va_end(list_args);
-	return (char_print);
+    int char_print = 0;
+    va_list list_args;
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    va_start(list_args, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            print_char(*format, &char_print);
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+            {
+                print_char(*format, &char_print);
+            }
+            else if (*format == 'c')
+            {
+                char c = va_arg(list_args, int);
+                print_char(c, &char_print);
+            }
+            else if (*format == 's')
+            {
+                char str = va_arg(list_args, char);
+                print_string(str, &char_print);
+            }
+            else if (*format == 'd' || *format == 'i') // Handle %d and %i specifier
+            {
+                int num = va_arg(list_args, int);
+                if (num < 0)
+                {
+                    print_char('-', &char_print);
+                    num = -num;
+                }
+                char num_str[20];
+                int len = 0;
+                while (num > 0)
+                {
+                    num_str[len] = (num % 10) + '0';
+                    num /= 10;
+                    len++;
+                }
+                if (len == 0)
+                {
+                    num_str[len] = '0';
+                    len++;
+                }
+                for (int i = len - 1; i >= 0; i--)
+                {
+                    print_char(num_str[i], &char_print);
+                }
+            }
+            else if (*format == 'u') // Handle %u specifier
+            {
+                unsigned int num = va_arg(list_args, unsigned int);
+                char num_str[20];
+                int len = 0;
+                while (num > 0)
+                {
+                    num_str[len] = (num % 10) + '0';
+                    num /= 10;
+                    len++;
+                }
+                if (len == 0)
+                {
+                    num_str[len] = '0';
+                    len++;
+                }
+                for (int i = len - 1; i >= 0; i--)
+                {
+                    print_char(num_str[i], &char_print);
+                }
+            }
+            else if (*format == 'o') // Handle %o specifier
+            {
+                unsigned int num = va_arg(list_args, unsigned int);
+                char num_str[20];
+                int len = 0;
+                while (num > 0)
+                {
+                    num_str[len] = (num % 8) + '0';
+                    num /= 8;
+                    len++;
+                }
+                if (len == 0)
+                {
+                    num_str[len] = '0';
+                    len++;
+                }
+                for (int i = len - 1; i >= 0; i--)
+                {
+                    print_char(num_str[i], &char_print);
+                }
+            }
+            else if (*format == 'x' || *format == 'X') // Handle %x and %X specifier
+            {
+                unsigned int num = va_arg(list_args, unsigned int);
+                char num_str[20];
+                int len = 0;
+                while (num > 0)
+                {
+                    int digit = num % 16;
+                    if (digit < 10)
+                        num_str[len] = digit + '0';
+                    else
+                        num_str[len] = (digit - 10) + ((*format == 'x') ? 'a' : 'A');
+                    num /= 16;
+                    len++;
+                }
+                if (len == 0)
+                {
+                    num_str[len] = '0';
+                    len++;
+                }
+                for (int i = len - 1; i >= 0; i--)
+                {
+                    print_char(num_str[i], &char_print);
+                }
+            }
+        }
+        format++;
+    }
+    va_end(list_args);
+    return char_print;
 }
